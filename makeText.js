ce3686cbfeac7f28d8e49e19fe41e870245e4665
Laura Moon <21319380+lauramoon@users.noreply.github.com>
2021-03-16 12:29:49 -0500

    step 2 complete

--- makeText.js
+++ makeText.js
@@ -1,2 +1,40 @@
 /** Command-line tool to generate Markov text. */
 
+const fs = require('fs');
+const axios = require('axios');
+const {MarkovMachine} = require('./markov');
+const argv = process.argv;
+
+function useFile(path) {
+    fs.readFile(path, 'utf8', function(err, data) {
+        if (err) {
+            console.log(`Error reading ${path}:`);
+            console.error("  " + err);
+            process.exit(1);
+        }
+        console.log(getMarkovText(data));
+      });
+}
+
+async function useURL(url) {
+    try {
+        res = await axios.get(url);
+        console.log(getMarkovText(res.data));
+    } catch (err) {
+        console.log(`Error fetching ${url}:`);
+        console.log("  " + err);
+    }
+}
+
+function getMarkovText(text) {
+    let mm = new MarkovMachine(text);
+    return mm.makeText();
+}
+
+if (argv[2].toLowerCase() === 'file') {
+    useFile(argv[3]);
+} else if (argv[2].toLowerCase() === 'url') {
+    useURL(argv[3]);
+} else {
+    console.log("Enter 'file' or 'url' after 'makeText.js'");
+}
